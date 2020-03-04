06ef1d1f9d34a5591a706998c23765be2212d26a
松本和奈 <matsumotokazuna@matsumotowananoMacBook-Pro.local>
2020-03-04 13:01:18 +0900

    [finish]3-6

---
+++ hoge.rb
@@ -0,0 +1,21 @@
+# 数値
+puts 100
+puts 100 + 5
+
+# 文字列
+puts "Rubyを学習"
+name = "鈴木さん"
+puts "Hello" + name
+puts "こんにちは#{name}"
+
+# 配列
+names = ["Perl","Ruby","Python","Swift"]
+puts names[1]
+
+# ハッシュ
+tall = {"太郎"=>185, "二郎"=>170, "花子"=>150}
+puts tall["太郎"]
+
+# シンボル
+tall = {:太郎=>185, :二郎=>170, :花子=>150}
+puts tall[:花子]
